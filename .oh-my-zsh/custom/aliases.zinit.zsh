7903802007c3d2b0af9df0545856edf20c71a285
Nelson de Witt <git@nelsonroberto.com>
2024-08-25 10:07:56 -0400

    Add reload and gi aliases

--- .oh-my-zsh/custom/aliases.zinit.zsh
+++ .oh-my-zsh/custom/aliases.zinit.zsh
@@ -7,3 +7,16 @@ alias lt='eza --tree --level=2 --icons --git'
 alias z='zellia a'
 alias gt='gittower ./'
 alias reload='source ~/.zshrc'
+
+function gi() { curl -sL https://www.toptal.com/developers/gitignore/api/$@ ;}
+
+_gitignoreio_get_command_list() {
+  curl -sL https://www.toptal.com/developers/gitignore/api/list | tr "," "\n"
+}
+
+_gitignoreio () {
+  compset -P '*,'
+  compadd -S '' `_gitignoreio_get_command_list`
+}
+
+compdef _gitignoreio gi
