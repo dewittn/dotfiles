ab238383d7aa5c7a180b14c472a7915c185609b2
Nelson/Roberto <git@nelsonroberto.com>
2025-03-20 17:09:03 -0400

    ✨ add basic nix-darwin config

---
+++ .config/nix/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#rcoto
+    darwinConfigurations."ndewitt" = nix-darwin.lib.darwinSystem {
+      modules = [ base dev ];
+    };
+    darwinConfigurations."rcoto" = nix-darwin.lib.darwinSystem {
+      modules = [ ./modules/base.nix ];
+    };
+  };
+}
