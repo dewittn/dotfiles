8eb2d58f4becf91c466af0a501a44bcddffa7bae
Nelson/Roberto <git@nelsonroberto.com>
2025-03-21 15:16:35 -0400

    bump system state version

--- .config/nix/darwinModules/programs/base.nix
+++ .config/nix/darwinModules/programs/base.nix
@@ -46,7 +46,7 @@
     
     # Used for backwards compatibility, please read the changelog before changing.
     # $ darwin-rebuild changelog
-    system.stateVersion = 4;
+    system.stateVersion = 5;
     # nixpkgs.hostPlatform = "aarch64-darwin";
     security.pam.services.sudo_local.touchIdAuth = true;
     
