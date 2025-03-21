1062f0f4ff0f10e26b8dfb2ba617f37cf9a6f677
Nelson/Roberto <git@nelsonroberto.com>
2025-03-21 08:30:53 -0400

    disabled option

--- .config/nix/darwinModules/programs/base.nix
+++ .config/nix/darwinModules/programs/base.nix
@@ -61,7 +61,7 @@
     nix.settings.experimental-features = "nix-command flakes";
     
     # Set Git commit hash for darwin-version.
-    system.configurationRevision = self.rev or self.dirtyRev or null;
+    # system.configurationRevision = self.rev or self.dirtyRev or null;
     
     # Used for backwards compatibility, please read the changelog before changing.
     # $ darwin-rebuild changelog
