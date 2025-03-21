75d71cc5a0f0b7127b6b4b1c44593819bde27067
Nelson/Roberto <git@nelsonroberto.com>
2025-03-21 12:39:11 -0400

    settings fix

--- .config/nix/darwinModules/programs/base.nix
+++ .config/nix/darwinModules/programs/base.nix
@@ -75,7 +75,7 @@
     # Used for backwards compatibility, please read the changelog before changing.
     # $ darwin-rebuild changelog
     system.stateVersion = 4;
-    nixpkgs.hostPlatform = "aarch64-darwin";
+    # nixpkgs.hostPlatform = "aarch64-darwin";
     security.pam.enableSudoTouchIdAuth = true;
     
     system.activationScripts.applications.text = 
