9dab62a2b59646517a95245050e2d7d84fdef9c7
Nelson/Roberto <git@nelsonroberto.com>
2025-03-21 15:03:35 -0400

    move inputs out of sub-modules

--- .config/nix/darwinModules/default.nix
+++ .config/nix/darwinModules/default.nix
@@ -1,13 +1,11 @@
 { pkgs, lib, inputs, ... }: {
   
-  specialArgs = { inherit inputs; };
   imports = [
-    ./programs/base.nix 
+    (import ./programs/base.nix inputs)
     # ./programs/photography.nix
     # ./programs/web-dev.nix
     # ./programs/writing.nix
   ];
 
   baseModule.enable = lib.mkDefault true;
-
 }
--- .config/nix/darwinModules/programs/base.nix
+++ .config/nix/darwinModules/programs/base.nix
@@ -1,4 +1,3 @@
-# {self, nixpkgs, nix-homebrew, homebrew-core, homebrew-cask, homebrew-bundle, ... }:
 { pkgs, lib, config, inputs, ... }: {
 
   options = {
@@ -23,29 +22,6 @@
         pkgs.zoxide
       ];
     
-    inputs.nix-homebrew = {inputs, ...}: {
-      # Install Homebrew under the default prefix
-      enable = true;
-    
-      # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-      enableRosetta = true;
-    
-      # User owning the Homebrew prefix
-      user = "dewittn";
-    
-      # Optional: Declarative tap management
-      taps = {
-        "homebrew/homebrew-core" = inputs.homebrew-core;
-        "homebrew/homebrew-cask" = inputs.homebrew-cask;
-        "homebrew/homebrew-bundle" = inputs.homebrew-bundle;
-      };
-    
-      # Optional: Enable fully-declarative tap management
-      #
-      # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
-      mutableTaps = false;
-    };
-    
     homebrew = {
       enable = true;
       brews = [
@@ -68,10 +44,6 @@
     services.nix-daemon.enable = true;
     nix.settings.experimental-features = "nix-command flakes";
     programs.zsh.enable = true;  # default shell on catalina
-    
-    # Set Git commit hash for darwin-version.
-    # system.configurationRevision = self.rev or self.dirtyRev or null;
-    system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
     
     # Used for backwards compatibility, please read the changelog before changing.
     # $ darwin-rebuild changelog
