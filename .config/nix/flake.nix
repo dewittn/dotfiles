6546e2d6d7f90af18b0a69c7951f93e09711246f
Nelson/Roberto <git@nelsonroberto.com>
2025-03-21 15:45:15 -0400

    removed nix-homebrew

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -27,20 +27,20 @@
       modules = [
         ./darwinModules 
       
-        nix-homebrew.darwinModules.nix-homebrew
-        {
-          nix-homebrew = {
-            # Install Homebrew under the default prefix
-            enable = true;
-      
-            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-            enableRosetta = true;
-      
-            # User owning the Homebrew prefix
-            user = "dewittn";
-            autoMigrate = true;
-          };
-        }
+      #   nix-homebrew.darwinModules.nix-homebrew
+      #   {
+      #     nix-homebrew = {
+      #       # Install Homebrew under the default prefix
+      #       enable = true;
+      # 
+      #       # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+      #       enableRosetta = true;
+      # 
+      #       # User owning the Homebrew prefix
+      #       user = "dewittn";
+      #       autoMigrate = true;
+      #     };
+      #   }
       ];
       # modules = [ base dev ];
       
