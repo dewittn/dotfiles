f91c9f6d13b8b3028a3d4aa6afd1bae9d75e8caf
Nelson/Roberto <git@nelsonroberto.com>
2025-03-21 15:45:15 -0400

    removed nix-homebrew

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -5,20 +5,9 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-    
-    # Optional: Declarative tap management
-    homebrew-core = {
-      url = "github:homebrew/homebrew-core";
-      flake = false;
-    };
-    homebrew-cask = {
-      url = "github:homebrew/homebrew-cask";
-      flake = false;
-    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nix-homebrew, homebrew-core, homebrew-cask, ... }:
+  outputs = inputs@{ self, nix-darwin,  ... }:
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#rcoto
@@ -26,21 +15,6 @@
       specialArgs = { inherit inputs; };
       modules = [
         ./darwinModules 
-      
-      #   nix-homebrew.darwinModules.nix-homebrew
-      #   {
-      #     nix-homebrew = {
-      #       # Install Homebrew under the default prefix
-      #       enable = true;
-      # 
-      #       # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-      #       enableRosetta = true;
-      # 
-      #       # User owning the Homebrew prefix
-      #       user = "dewittn";
-      #       autoMigrate = true;
-      #     };
-      #   }
       ];
       # modules = [ base dev ];
       
