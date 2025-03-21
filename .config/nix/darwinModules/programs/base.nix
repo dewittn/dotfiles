f80c10be029dd04be2ec0c6037df7aedbe780cdb
Nelson/Roberto <git@nelsonroberto.com>
2025-03-21 13:31:51 -0400

    specialArgs

--- .config/nix/darwinModules/programs/base.nix
+++ .config/nix/darwinModules/programs/base.nix
@@ -71,7 +71,7 @@
     
     # Set Git commit hash for darwin-version.
     # system.configurationRevision = self.rev or self.dirtyRev or null;
-    system.configurationRevision = self.rev or self.dirtyRev or null;
+    system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
     
     # Used for backwards compatibility, please read the changelog before changing.
     # $ darwin-rebuild changelog
