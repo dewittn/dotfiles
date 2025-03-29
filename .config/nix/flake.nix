1fa17ae160a637e96591a2fc08e6a4acea23ce30
Nelson/Roberto <git@nelsonroberto.com>
2025-03-29 17:35:43 -0400

    add vm config

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -58,6 +58,44 @@
           }
         ];
       };
+
+      darwinConfigurations."vm" = nix-darwin.lib.darwinSystem {
+        specialArgs = { 
+          inherit (inputs) self; 
+          inherit (inputs) nix-darwin; 
+          inherit (inputs) nix-homebrew; 
+          inherit (inputs) homebrew-core; 
+          inherit (inputs) homebrew-cask; 
+        };
+          
+        modules = [
+          ./roles/vm
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              # Install Homebrew under the default prefix
+              enable = true;
+          
+              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+              enableRosetta = true;
+          
+              # User owning the Homebrew prefix
+              user = "dewittn";
+          
+              # Optional: Declarative tap management
+              taps = {
+                "homebrew/homebrew-core" = homebrew-core;
+                "homebrew/homebrew-cask" = homebrew-cask;
+              };
+          
+              # Optional: Enable fully-declarative tap management
+              #
+              # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+              mutableTaps = false;
+            };
+          }
+        ];
+      };
       
       darwinConfigurations."server" = nix-darwin.lib.darwinSystem {
         modules = [ ./modules/base.nix ];
--- .config/nix/programs/mas/default.nix
+++ .config/nix/programs/mas/default.nix
@@ -15,7 +15,7 @@
     programmingMasModule.enable = lib.mkDefault true;
     writtingMasModule.enable = lib.mkDefault true;
     personalMasModule.enable = lib.mkDefault true;
-    photographyMasModule.enable = lib.mkDefault true;
+    # photographyMasModule.enable = lib.mkDefault true;
   };
 
 }
