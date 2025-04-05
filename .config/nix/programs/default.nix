48c58ec08ee3bf08cbd1ceb91c879c03be4a581c
Nelson/Roberto <git@nelsonroberto.com>
2025-04-05 09:00:28 -0400

    removed mas submodule

--- .config/nix/programs/default.nix
+++ .config/nix/programs/default.nix
@@ -9,4 +9,9 @@
   baseModule.enable = lib.mkDefault true;
   programmingPkgsModule.enable = lib.mkDefault false;
   masModule.enable = lib.mkDefault false;
+  
+  programmingMasModule.enable = lib.mkDefault true;
+  writtingMasModule.enable = lib.mkDefault true;
+  personalMasModule.enable = lib.mkDefault true;
+  photographyMasModule.enable = lib.mkDefault true;
 }
--- .config/nix/programs/mas/default.nix
+++ .config/nix/programs/mas/default.nix
@@ -6,16 +6,5 @@
     ./personal.nix
     ./writing.nix
   ];
-  
-  options = {
-    masModule.enable = lib.mkEnableOption "enables masModule";
-  };
-  
-  config = lib.mkIf config.masModule.enable {
-    programmingMasModule.enable = lib.mkDefault true;
-    writtingMasModule.enable = lib.mkDefault true;
-    personalMasModule.enable = lib.mkDefault true;
-    # photographyMasModule.enable = lib.mkDefault true;
-  };
 
 }
