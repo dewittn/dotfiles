4a42a80dc17330e01d5d93e4b1aeb746924d830f
Nelson/Roberto <git@nelsonroberto.com>
2025-07-31 14:48:46 -0400

    ✨ add yazi

--- .oh-my-zsh/custom/aliases.zinit.zsh
+++ .oh-my-zsh/custom/aliases.zinit.zsh
@@ -21,3 +21,12 @@ _gitignoreio () {
 }
 
 compdef _gitignoreio gi
+
+export EDITOR="nvim"
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
